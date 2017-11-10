e2f1b787bd9b59ee1a0de4374f14280ad86b2b1c
SURESH RAJ PERIANNAN <sureshraj@live.com.au>
2017-11-10 16:56:34 +1100

    Create Excerise3.py

---
+++ Excerise3.py
@@ -0,0 +1,27 @@
+# Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error message. If the score is between 0.0 and 1.0, print a grade using the following table:
+
+# Score   Grade
+# >= 0.9     A
+# >= 0.8     B
+# >= 0.7     C
+# >= 0.6     D
+# < 0.6    F
+
+score = input('Enter Score : ')
+score = float(score)
+if score < 1.0 :
+	try:
+		if score >= 0.9 :
+			print('A')
+		elif score >= 0.8 :
+			print('B')
+		elif score >= 0.7 :
+			print('C')
+		elif score >= 0.6 :
+			print('D')
+		elif score < 0.6 :
+			print('F')
+	except :
+		print('Bad score')
+else:
+	print('Bad score')
