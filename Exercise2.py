b4dcf8e7b766ef401b261ee5c3b943c3f48c9253
SURESH RAJ PERIANNAN <sureshraj@live.com.au>
2017-11-10 16:36:08 +1100

    Update Exercise2.py

--- Exercise2.py
+++ Exercise2.py
@@ -1,8 +1,17 @@
+# pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program. The following shows two executions of the program:
+
 hours = input('Enter Hours: ')
 rate = input('Enter Rate: ')
 try:
     hours = float(hours)
     rate = float(rate)
-    print('Both are success')
+    print('Both are integer')
 except:
-    print('Error')
+    print('Error, please enter numeric input')
+
+hours = input('Enter Hours: ')
+try:
+    hours = float(hours)
+    print('Hours is integer')
+except:
+    print('Error, please enter numeric input')
